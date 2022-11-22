a0418c19dfcd1f9a38bda6a41f9abf3753dab8ad
Dimentiu <dimentiu4@gmail.com>
2022-11-22 10:03:37 +0200

    DZ.14

--- bookstore1/admin.py
+++ bookstore1/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Author, Quote
+
+
+@admin.register(Author)
+class AuthorModelAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description']
+    fields = ['name', 'description']
+    search_fields = ["name"]
+
+
+@admin.register(Quote)
+class BookModelAdmin(admin.ModelAdmin):
+    list_display = ['message', 'author']
+    fields = ['message', 'author']
+    raw_id_fields = ['author', ]
