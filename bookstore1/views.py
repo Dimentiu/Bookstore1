d6a4b424b3a449e8a0afecffa9979c30455e0504
Dimentiu <dimentiu4@gmail.com>
2022-11-26 10:39:27 +0200

    ДЗ 16. Django redis cache

--- bookstore1/views.py
+++ bookstore1/views.py
@@ -1,8 +1,12 @@
-import datetime
+from django.http import Http404
 from django.shortcuts import render
-from django.utils import timezone
+from django.views import generic
+
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from .forms import ReminderForm
+from .models import Author, Quote
 from .tasks import send_mail
+from django.views.decorators.cache import cache_page
 
 
 def create_reminder(request):
@@ -12,19 +16,17 @@ def create_reminder(request):
             email = reminder_form.cleaned_data['email']
             text = reminder_form.cleaned_data['text']
             data = reminder_form.cleaned_data['datetime']
-            today_now = timezone.now()
-            if today_now < data < today_now + datetime.timedelta(days=2):
-                message = "Message has sent successfully!"
-                send_mail.apply_async((text, email), eta=data)
-                return render(
-                    request,
-                    'bookstore1/create_reminder.html',
-                    {
-                        'reminder_form': reminder_form,
-                        'message': message,
+            message = "Message has sent successfully!"
+            send_mail.apply_async((text, email), eta=data)
+            return render(
+                request,
+                'bookstore1/create_reminder.html',
+                {
+                    'reminder_form': reminder_form,
+                    'message': message,
 
-                    }
-                )
+                }
+            )
     else:
         reminder_form = ReminderForm()
     return render(
@@ -34,3 +36,30 @@ def create_reminder(request):
             'reminder_form': reminder_form,
         }
     )
+
+
+@cache_page(1)
+def quote_list(request):
+    quotes_list = Quote.objects.select_related('author').all()
+    page = request.GET.get('page', 1)
+    paginator = Paginator(quotes_list, 150)
+    try:
+        page_obj = paginator.page(page)
+    except PageNotAnInteger:
+        page_obj = paginator.page(1)
+    except EmptyPage:
+        raise Http404
+    context = {'page_obj': page_obj}
+    return render(request, 'bookstore1/pagination_quotes.html', context)
+
+
+class AuthorListView(generic.ListView):
+    model = Author
+    queryset = Author.objects.all()
+    paginate_by = 200
+    template_name = 'bookstore1/pagination_authors.html'
+
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
+    template_name = 'bookstore1/detail_author.html'
